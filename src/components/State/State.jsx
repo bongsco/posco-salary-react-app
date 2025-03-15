89a1cfbf4469bf72c5683cab5495a171b901d33f
eu2525 <eu2525@khu.ac.kr>
2025-03-15 20:47:17 +0900

    ♻️ refactor using css module

--- src/components/State/State.jsx
+++ src/components/State/State.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types'; // PropTypes import
-import './State.css';
+import PropTypes from 'prop-types';
+import styles from './state.module.css';
 
 const statusMap = {
-  complete: { text: '완료', className: 'state-label complete' },
-  working: { text: '작업중', className: 'state-label working' },
-  warning: { text: '조치 필요', className: 'state-label warning' },
+  complete: { text: '완료', className: 'complete' },
+  working: { text: '작업중', className: 'working' },
+  warning: { text: '조치 필요', className: 'warning' },
 };
 
 export default function State({ status }) {
   const { text: labelText, className: labelClass } = statusMap[status];
 
-  return <div className={labelClass}>{labelText}</div>;
+  return (
+    <div className={`${styles['state-label']} ${styles[labelClass]}`}>
+      {labelText}
+    </div>
+  );
 }
 
 State.propTypes = {
