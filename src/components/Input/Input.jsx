d93bede7065a4fc8bded9f3adf816bc208a4827c
Godbell <jjongha134@gmail.com>
2025-03-18 12:40:11 +0900

    ♻️ Remove redundant condition check and code

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,67 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './input.module.css';
 
-function Input({ mode = 'primary', placeholder, label, onFocus, onBlur }) {
-  const modeLabels = {
-    error: '여기에 입력 오류와 관련된 메시지를 입력해 주세요.',
-    ok: '여기에 메시지를 입력해 주세요.',
-    default: '여기에 입력 관련 메시지를 입력하세요.',
-  };
-
-  const [isFocused, setIsFocused] = useState(false);
+function Input({
+  mode = 'default',
+  placeholder = '',
+  label,
+  onFocus,
+  onBlur,
+  onChange,
+}) {
   const [value, setValue] = useState('');
-
-  useEffect(() => {
-    if (placeholder) {
-      setValue(placeholder);
-      setIsFocused(true);
-    } else {
-      setValue('');
-    }
-  }, [placeholder]);
 
   return (
     <div className={styles['input-box-container']}>
       <input
         type="text"
         value={value}
-        className={`${styles['input-box']} ${styles[mode]} ${isFocused && mode === 'primary' ? styles.focused : ''}`}
+        className={`${styles['input-box']} ${styles[mode]}`}
         placeholder={placeholder}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
         onFocus={(e) => {
-          setIsFocused(true);
           if (onFocus) onFocus(e);
         }}
         onBlur={(e) => {
-          if (!value) setIsFocused(false);
           if (onBlur) onBlur(e);
         }}
       />
-      {value && (
+      {label ? (
         <span
           className={`${styles['input-box-label']} ${styles[mode]} ${styles.visible}`}
         >
-          {label || modeLabels[mode] || modeLabels.default}
+          {label}
         </span>
-      )}
+      ) : null}
     </div>
   );
 }
 
 Input.defaultProps = {
-  mode: 'primary',
-  label: '여기에 입력 오류와 관련된 메시지를 입력해 주세요.',
+  mode: 'default',
+  label: '',
+  placeholder: '',
   onFocus: null,
   onBlur: null,
 };
 
 Input.propTypes = {
-  mode: PropTypes.oneOf(['primary', 'error', 'ok']),
-  placeholder: PropTypes.string.isRequired,
+  mode: PropTypes.oneOf(['default', 'error', 'ok']),
   label: PropTypes.string,
+  placeholder: PropTypes.string,
   onFocus: PropTypes.func,
   onBlur: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default Input;
--- src/components/Input/Input.stories.jsx
+++ src/components/Input/Input.stories.jsx
@@ -4,19 +4,22 @@ import Input from './Input';
 export default {
   title: 'UI/Input',
   component: Input,
+  tags: ['autodocs'],
   argTypes: {
     mode: { control: { type: 'select', options: ['primary', 'error', 'ok'] } },
     placeholder: { control: 'text' },
     label: 'text',
+  },
+  args: {
     onFocus: fn(),
     onBlur: fn(),
+    onChange: fn(),
   },
-  tags: ['autodocs'],
 };
 
 export const Default = {
   args: {
-    mode: 'primary',
+    mode: 'default',
     placeholder: '플레이스 홀더 PlaceHolder',
     label: '여기에 입력 오류와 관련된 메시지를 입력해 주세요.',
   },
