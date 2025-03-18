1ca15475b0f9ab8957022e688b1d2144d3586f48
eu2525 <eu2525@khu.ac.kr>
2025-03-18 15:13:08 +0900

    📝 update onClick props in storybook

--- src/components/Switch/Switch.stories.jsx
+++ src/components/Switch/Switch.stories.jsx
@@ -1,21 +1,17 @@
+import { fn } from '@storybook/test';
 import Switch from './Switch';
 
 export default {
   title: 'UI/Switch',
   component: Switch,
   tags: ['autodocs'],
-  argTypes: {
-    initialOn: { control: 'boolean' },
+  args: {
+    onClick: fn(),
   },
 };
 
-const data = { toggle1: false };
 export const Default = {
   args: {
-    initialOn: data.toggle1,
-    onClick: (value) => {
-      data.toggle1 = value;
-      console.log(`새로운 값: ${value}`);
-    },
+    initialOn: false,
   },
 };
