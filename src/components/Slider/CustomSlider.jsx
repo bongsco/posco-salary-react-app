009fbcca9cdb857477302543eb4103861b477fc4
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-03-24 17:19:22 +0900

    ♻️ Convert UI/Slider to fully controlled component (#99)
    * 🎨 Change props and state
    
    * 🎨 Change param name
    
    * 🎨 Change storybook

--- src/components/Slider/CustomSlider.jsx
+++ src/components/Slider/CustomSlider.jsx
@@ -1,42 +1,35 @@
-import { useState } from 'react';
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 import PropTypes from 'prop-types';
 import styles from './slider.module.css';
 
 export default function CustomSlider({
-  initialMin,
-  initialMax,
+  min,
+  max,
   minLowerBound,
   maxUpperBound,
   step,
   onChange,
 }) {
-  const [range, setRange] = useState([initialMin, initialMax]);
-  const handleSliderChange = (value) => {
-    onChange(value[0], value[1]);
-    setRange(value);
-  };
-
   return (
     <div className={`${styles.slider_container}`}>
-      <div className={`${styles.slider_value}`}>{range[0]}</div>
+      <div className={`${styles.slider_value}`}>{min}</div>
       <RangeSlider
         min={minLowerBound}
         max={maxUpperBound}
         step={step}
-        value={range}
-        onInput={(value) => handleSliderChange(value)}
+        value={[min, max]}
+        onInput={(value) => onChange(value[0], value[1])}
         rangeSlideDisabled
       />
-      <div className={`${styles.slider_value}`}>{range[1]}</div>
+      <div className={`${styles.slider_value}`}>{max}</div>
     </div>
   );
 }
 
 CustomSlider.propTypes = {
-  initialMin: PropTypes.number.isRequired,
-  initialMax: PropTypes.number.isRequired,
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
   minLowerBound: PropTypes.number.isRequired,
   maxUpperBound: PropTypes.number.isRequired,
   step: PropTypes.number.isRequired,
