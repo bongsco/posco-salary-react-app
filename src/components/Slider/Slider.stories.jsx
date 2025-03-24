e9815b0b803f62f0b03249d75243864f822201ae
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-03-24 12:29:26 +0900

    🎨 Change storybook

--- src/components/Slider/Slider.stories.jsx
+++ src/components/Slider/Slider.stories.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react';
+import { fn } from '@storybook/test';
+import PropTypes from 'prop-types';
 import CustomSlider from './CustomSlider';
 
 export default {
   title: 'UI/CustomSlider',
   component: CustomSlider,
   tags: ['autodocs'],
+  argTypes: {
+    minLowerBound: { control: 'number' },
+    maxUpperBound: { control: 'number' },
+    step: { control: 'number' },
+  },
+  args: {
+    onChange: fn(),
+  },
 };
 
-function Template() {
+function Template({ minLowerBound, maxUpperBound, step, onChange }) {
   const [min, setMin] = useState(10);
   const [max, setMax] = useState(90);
 
@@ -15,15 +25,28 @@ function Template() {
     <CustomSlider
       min={min}
       max={max}
-      minLowerBound={0}
-      maxUpperBound={200}
-      step={1}
+      minLowerBound={minLowerBound}
+      maxUpperBound={maxUpperBound}
+      step={step}
       onChange={(lower, upper) => {
         setMin(lower);
         setMax(upper);
+        onChange(lower, upper);
       }}
     />
   );
 }
 
+Template.propTypes = {
+  minLowerBound: PropTypes.number.isRequired,
+  maxUpperBound: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
 export const Default = Template.bind();
+Default.args = {
+  minLowerBound: 0,
+  maxUpperBound: 200,
+  step: 1,
+};
