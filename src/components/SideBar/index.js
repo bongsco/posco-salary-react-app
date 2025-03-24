d695752f80a2ab062f2574ddecf9c878b2e3d1e3
Godbell <jjongha134@gmail.com>
2025-03-24 17:54:49 +0900

    ♻️ Refactor Layout Components (#110)
    * ♻️ Optimize layout structure and refactor sidebar
    
    * ✨ Refactor Navbar and impl toggleSidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,1 +1,7 @@
-export { default } from './SideBar';
+import SideBar from './SideBar';
+import Category from './Category';
+import SubItem from './SubItem';
+import Item from './Item';
+
+export default SideBar;
+export { Item, Category, SubItem };
