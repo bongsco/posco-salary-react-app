95fad3882000091686e58e3ddb210e2c491c3cf1
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-03-28 17:13:22 +0900

    🎨 Change sample data

--- src/components/TimeLine/TimeLine.stories.jsx
+++ src/components/TimeLine/TimeLine.stories.jsx
@@ -19,42 +19,57 @@ function Template({ minDate, maxDate }) {
     maxDate instanceof Date ? maxDate : new Date(maxDate);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const data = [
-    ['2024 BASE_UP 1', new Date(2024, 0, 1), new Date(2024, 11, 31)],
+    ['2023 BASE_UP 1', new Date(2023, 0, 1), new Date(2023, 11, 31)],
     [
-      '2023 PROMOTED_SALARY_ADJUSTMENT 2',
-      new Date(2023, 8, 1),
-      new Date(2024, 2, 29),
+      '2022 PROMOTED_SALARY_ADJUSTMENT 2',
+      new Date(2022, 9, 1),
+      new Date(2023, 3, 31),
     ],
     [
-      '2023 PROMOTED_SALARY_ADJUSTMENT 1',
-      new Date(2023, 3, 1),
-      new Date(2023, 7, 31),
+      '2022 PROMOTED_SALARY_ADJUSTMENT 1',
+      new Date(2022, 4, 1),
+      new Date(2022, 9, 31),
     ],
     [
-      '2023 ANNUAL_SALARY_ADJUSTMENT',
-      new Date(2023, 0, 1),
-      new Date(2023, 11, 31),
-    ],
-    ['2022 BASE_UP 1', new Date(2022, 0, 1), new Date(2022, 11, 31)],
-    ['2022 MID_YEAR_ADJUSTMENT', new Date(2022, 5, 1), new Date(2022, 7, 31)],
-    [
-      '2022 PROMOTED_SALARY_ADJUSTMENT',
+      '2022 ANNUAL_SALARY_ADJUSTMENT 2',
       new Date(2022, 2, 1),
-      new Date(2022, 4, 30),
+      new Date(2022, 11, 31),
     ],
-    ['2021 BASE_UP 1', new Date(2021, 0, 1), new Date(2021, 11, 31)],
-    ['2021 PROMOTION_EVENT', new Date(2021, 6, 1), new Date(2021, 9, 30)],
-    ['2021 SPECIAL_ADJUSTMENT', new Date(2021, 3, 1), new Date(2021, 5, 30)],
-    ['2020 BASE_UP 1', new Date(2020, 0, 1), new Date(2020, 11, 31)],
-    ['2020 COVID_BONUS', new Date(2020, 5, 1), new Date(2020, 6, 31)],
-    ['2019 BASE_UP 1', new Date(2019, 0, 1), new Date(2019, 11, 31)],
     [
-      '2019 PERFORMANCE_ADJUSTMENT',
-      new Date(2019, 3, 1),
-      new Date(2019, 5, 30),
+      '2022 ANNUAL_SALARY_ADJUSTMENT 1',
+      new Date(2022, 0, 1),
+      new Date(2022, 11, 31),
     ],
-    ['2018 BASE_UP 1', new Date(2018, 0, 1), new Date(2018, 11, 31)],
-    ['2018 EARLY_PROMOTION_EVENT', new Date(2018, 2, 1), new Date(2018, 4, 31)],
+    [
+      '2021 PROMOTED_SALARY_ADJUSTMENT 2',
+      new Date(2021, 9, 1),
+      new Date(2022, 3, 31),
+    ],
+    [
+      '2021 PROMOTED_SALARY_ADJUSTMENT 1',
+      new Date(2021, 4, 1),
+      new Date(2021, 9, 31),
+    ],
+    [
+      '2021 ANNUAL_SALARY_ADJUSTMENT 2',
+      new Date(2021, 2, 1),
+      new Date(2021, 11, 31),
+    ],
+    [
+      '2021 ANNUAL_SALARY_ADJUSTMENT 1',
+      new Date(2021, 0, 1),
+      new Date(2021, 11, 31),
+    ],
+    [
+      '2020 ANNUAL_SALARY_ADJUSTMENT 1',
+      new Date(2020, 0, 1),
+      new Date(2020, 11, 31),
+    ],
+    [
+      '2019 ANNUAL_SALARY_ADJUSTMENT 1',
+      new Date(2019, 0, 1),
+      new Date(2019, 11, 31),
+    ],
   ];
   return (
     <CustomTimeLine
